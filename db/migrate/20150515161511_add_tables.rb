49bde03d1938c2a13d2887a3676b1dd1eb902cf3
Axel <kurtastraube@gmail.com>
2015-05-15 09:23:15 -0700

    Add initial files.

---
+++ db/migrate/20150515161511_add_tables.rb
@@ -0,0 +1,14 @@
+class AddTables < ActiveRecord::Migration
+  def change
+    create_table :bands do |t|
+      t.string :name
+    end
+    create_table :venues do |t|
+      t.string :name
+    end
+    create_table :bands_venues, id: false do |t|
+      t.belongs_to :band, index: true
+      t.belongs_to :venue, index: true
+    end
+  end
+end
